75fe66640259984210853c109433ee71e30498b6
Daniel Rocha <danielsantosr.rj@gmail.com>
2017-03-01 18:42:43 -0300

    desenv

---
+++ casadocodigo/app/routes/produtos.js
@@ -0,0 +1,8 @@
+module.exports = function(app) {
+
+  app.get('/produtos', function(req, res) {
+  	console.log("listando...");
+  	res.render("produtos/lista");
+  });
+
+}
--- casadocodigo/config/express.js
+++ casadocodigo/config/express.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 app.set('view engine','ejs');
+app.set('views','./app/views');
 
 module.exports = function() {
   console.log("O modulo esta sendo carregado");
